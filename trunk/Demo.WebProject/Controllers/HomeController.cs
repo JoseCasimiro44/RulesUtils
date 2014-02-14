4f322043fe09e97406bd3515959205a02dc7cd92
JoseCasimiro44 <josecasimiro44@gmail.com>
2014-02-14 10:35:12 +0000

    Demo.WebProject
    + Add: A Web Application project to use as a test environment for rules

---
+++ trunk/Demo.WebProject/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Demo.WebProject.Models;
+
+namespace Demo.WebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+          HomeModel model = new HomeModel();
+          return View(model);
+        }
+    }
+}
---
+++ trunk/Demo.WebProject/Models/HomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Demo.WebProject.Properties;
+
+namespace Demo.WebProject.Models
+{
+  public class HomeModel
+  {
+    [Display(ResourceType = typeof(Texts), Name="HelloWorld")]
+    public string Name { get{ return "I'm a house!";}}
+  }
+}
